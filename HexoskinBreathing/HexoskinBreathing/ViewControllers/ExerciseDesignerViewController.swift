fb52ee6282483c6e1221cd51985537c146aa4c2f
Matthew Richardson <Matthew@Matthews-MacBook-Pro.local>
2017-03-27 20:14:42 -0500

    Starting fixing exercise designer - still needs to be visually updated to look good

--- HexoskinBreathing/HexoskinBreathing/ViewControllers/ExerciseDesignerViewController.swift
+++ HexoskinBreathing/HexoskinBreathing/ViewControllers/ExerciseDesignerViewController.swift
@@ -16,8 +16,7 @@ class ExerciseDesignerViewController: MRRViewController, UITableViewDelegate, UI
     
     
     // outlets
-    @IBOutlet weak var arrowImageView: UIImageView!
-    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var editTitleLabel: UILabel!
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var metronomeTitleLabel: UILabel!
     @IBOutlet weak var durationLabel: UILabel!
@@ -29,7 +28,10 @@ class ExerciseDesignerViewController: MRRViewController, UITableViewDelegate, UI
     @IBOutlet weak var metronomeSwitch: UISwitch!
     @IBOutlet weak var metronomeLabel: UILabel!
     @IBOutlet weak var continueButton: UIButton!
-
+    
+    var editViews: [UIView]!
+    var rowBeingEdited: Int = -1;
+    
     // indicate signed in status
     var signedIn: Bool!
     
@@ -40,7 +42,9 @@ class ExerciseDesignerViewController: MRRViewController, UITableViewDelegate, UI
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "Configure"
+        self.title = "Create Exercise"
+        
+        editViews = [editTitleLabel, durationLabel, durationStepper, durationCountLabel, cyclesLabel, cyclesStepper, cyclesCountLabel];
         
         self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         
@@ -51,6 +55,8 @@ class ExerciseDesignerViewController: MRRViewController, UITableViewDelegate, UI
         prepareMetronomeSwitch();
         setTextColorOfLabels();
         
+        // set color for edit title label
+        editTitleLabel.textColor = Constants.basicTextColor;
 
         continueButton.setTitleColor(Constants.basicTextColor, for: .normal);
         continueButton.backgroundColor = Constants.basicButtonBackgroundColor;
@@ -62,16 +68,15 @@ class ExerciseDesignerViewController: MRRViewController, UITableViewDelegate, UI
         tableView.delegate = self;
         tableView.dataSource = self;
         tableView.tableFooterView = UIView()
-        tableView.backgroundColor = .clear;
+        tableView.backgroundColor = .white;
         tableView.layer.borderColor = Constants.basicTextColor.cgColor;
         tableView.layer.borderWidth = 2;
         tableView.layer.cornerRadius = 8;
         tableView.bounces = false;
         
-        // create a tap gesture recognizer for the image view
-        let recognizer = UITapGestureRecognizer(target: self, action: #selector(ExerciseDesignerViewController.arrowImageViewTapped));
-        arrowImageView.isUserInteractionEnabled = true;
-        arrowImageView.addGestureRecognizer(recognizer);
+        // fade all views initially until a row is selected and the row should be edited
+        fadeAllEditViews();
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -125,31 +130,48 @@ class ExerciseDesignerViewController: MRRViewController, UITableViewDelegate, UI
     
     @IBAction func ContinuePressed(_ sender: Any) {
         // create the exercise based on the stepper values and then push to exercise view controller
-        let exercise = BreathingExercise();
-        exercise.addExerciseSets(exerciseSets: exerciseSets);
-//        let exercise = BreathingExercise(duration: durationStepper.value, cycles: Int(cyclesStepper.value));
-        
-        // instantiate the view controller from interface builder
-        let storyboard = UIStoryboard(name: "Main", bundle: nil);
-        let viewController = storyboard.instantiateViewController(withIdentifier: "exerciseViewController") as? ExerciseViewController;
-        
-        // set the authorization details and exercise members for the exercise view controller
-        viewController?.playMetronome = metronomeSwitch.isOn;
-        viewController?.accessToken = accessToken;
-        viewController?.tokenType = tokenType;
-        viewController?.exercise = exercise;
-        viewController?.signedIn = signedIn; 
-        self.navigationController?.pushViewController(viewController!, animated: true);
+        if exerciseSets.count != 0 {
+            let exercise = BreathingExercise();
+            exercise.addExerciseSets(exerciseSets: exerciseSets);
+            //        let exercise = BreathingExercise(duration: durationStepper.value, cycles: Int(cyclesStepper.value));
+            
+            // instantiate the view controller from interface builder
+            let storyboard = UIStoryboard(name: "Main", bundle: nil);
+            let viewController = storyboard.instantiateViewController(withIdentifier: "exerciseViewController") as? ExerciseViewController;
+            
+            // set the authorization details and exercise members for the exercise view controller
+            viewController?.playMetronome = metronomeSwitch.isOn;
+            viewController?.accessToken = accessToken;
+            viewController?.tokenType = tokenType;
+            viewController?.exercise = exercise;
+            viewController?.signedIn = signedIn;
+            self.navigationController?.pushViewController(viewController!, animated: true);
+        } else {
+            let alert = UIAlertController(title: "Exercise is empty", message: "Please add instructions.", preferredStyle: .alert);
+            alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil));
+            self.present(alert, animated: true, completion: nil);
+        }
     }
     
     @IBAction func durationStepperPressed(_ sender: Any) {
         durationCountLabel.text = "\(Int(durationStepper.value))s";
+        
+        // check which cell is being edited and adjust that text and the exerciseSets entry
+        let cell = tableView.cellForRow(at: IndexPath(row: rowBeingEdited, section: 0));
+        let cycles = exerciseSets[rowBeingEdited].1;
+        exerciseSets[rowBeingEdited] = (durationStepper.value, cycles);
+        cell?.textLabel?.text = "\(Int(rowBeingEdited+1)). Inhale \(Int(exerciseSets[rowBeingEdited].0)) s, Exhale \(Int(exerciseSets[rowBeingEdited].0)) s   x   \(exerciseSets[rowBeingEdited].1)";
     }
     
     @IBAction func cycleStepperPressed(_ sender: Any) {
         cyclesCountLabel.text = "\(Int(cyclesStepper.value))x";
+        
+        let cell = tableView.cellForRow(at: IndexPath(row: rowBeingEdited, section: 0));
+        let duration = exerciseSets[rowBeingEdited].0;
+        exerciseSets[rowBeingEdited] = (duration, Int(cyclesStepper.value));
+        cell?.textLabel?.text = "\(Int(rowBeingEdited+1)). Inhale \(Int(exerciseSets[rowBeingEdited].0)) s, Exhale \(Int(exerciseSets[rowBeingEdited].0)) s   x   \(exerciseSets[rowBeingEdited].1)";
     }
-
+    
     @IBAction func metronomeSwitch(_ sender: Any) {
         if metronomeSwitch.isOn {
             metronomeLabel.text = "On";
@@ -172,7 +194,7 @@ class ExerciseDesignerViewController: MRRViewController, UITableViewDelegate, UI
     // tableview delegate methods
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return exerciseSets.count
+        return exerciseSets.count + 1;
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -181,39 +203,74 @@ class ExerciseDesignerViewController: MRRViewController, UITableViewDelegate, UI
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: UITableViewCell = self.tableView.dequeueReusableCell(withIdentifier: "cell")! as UITableViewCell
-        cell.textLabel?.text = "\(Int(indexPath.row+1)). Inhale \(Int(exerciseSets[indexPath.row].0)) s, Exhale \(Int(exerciseSets[indexPath.row].0)) s   x   \(exerciseSets[indexPath.row].1)";
-        cell.backgroundColor = .clear
-        cell.textLabel?.textColor = Constants.basicTextColor;
+        if indexPath.row == exerciseSets.count {
+            // make a row that indicates they should press it to add a new instructions
+            cell.textLabel?.text = "Add instruction set";
+        } else {
+            cell.textLabel?.text = "\(Int(indexPath.row+1)). Inhale \(Int(exerciseSets[indexPath.row].0)) s, Exhale \(Int(exerciseSets[indexPath.row].0)) s   x   \(exerciseSets[indexPath.row].1)";
+        }
+        cell.textLabel?.textColor = .black;
+        
+        // check to see if this the row that is being edited
+        // if so, set is as selected
+        if indexPath.row == rowBeingEdited {
+            cell.backgroundColor = UIColor.lightGray;
+        } else {
+            cell.backgroundColor = UIColor.white;
+        }
+        
         return cell;
     }
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
+            
+            // fade all edit views
+            self.fadeAllEditViews();
+            
+            // clear the row being edited
+            rowBeingEdited = -1;
+            
             // deleting row at this indexPath
             removeExerciseSet(indexPath: indexPath);
         }
     }
     
     func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        if indexPath.row == exerciseSets.count {
+            return false;
+        }
         return true;
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let alert = UIAlertController(title: "Delete exercise set?", message: "Inhale \(Int(exerciseSets[indexPath.row].0)) s, Exhale \(Int(exerciseSets[indexPath.row].0)) s   x   \(exerciseSets[indexPath.row].1)", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "Delete", style: .destructive, handler: {
-            _ in
-            // delete the action exercise set at this index
-            DispatchQueue.main.async {
-                self.removeExerciseSet(indexPath: indexPath);
+        if rowBeingEdited != indexPath.row {
+            
+            // unfade edit views
+            self.revealAllEditViews();
+            editTitleLabel.text = "Edit instructions in row \(indexPath.row+1)";
+            
+            // set which row is being edited
+            rowBeingEdited = indexPath.row;
+            
+            if indexPath.row == exerciseSets.count {
+                // add set to exerciseSets
+                exerciseSets.append((durationStepper.value, Int(cyclesStepper.value)));
+            } else {
+                // set the stepper values to that of the instruction being currently edited
+                durationStepper.value = exerciseSets[rowBeingEdited].0;
+                durationCountLabel.text = "\(Int(durationStepper.value))s";
+                cyclesStepper.value = Double(exerciseSets[rowBeingEdited].1);
+                cyclesCountLabel.text = "\(Int(cyclesStepper.value))x";
             }
-        }));
-        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: {
-            _ in
-            DispatchQueue.main.async {
-                self.tableView.deselectRow(at: indexPath, animated: true);
-            }
-        }));
-        self.present(alert, animated: true, completion: nil);
+            
+        } else {
+            rowBeingEdited = -1;
+            self.fadeAllEditViews();
+        }
+
+        // reload data to observe changes
+        tableView.reloadData();
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
@@ -232,10 +289,18 @@ class ExerciseDesignerViewController: MRRViewController, UITableViewDelegate, UI
         tableView.reloadData()
     }
     
-    func arrowImageViewTapped() {
-        addExerciseToList();
+    func fadeAllEditViews() {
+        for view in editViews {
+            view.alpha = 0;
+        }
     }
     
+    func revealAllEditViews() {
+        for view in editViews {
+            view.alpha = 1.0;
+        }
+    }
+
 
 
 }
